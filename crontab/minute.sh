9f950b90473de1b79a67cc193e7682e7b1d97f0d
Lazy <hi@cnlazy.cn>
2020-03-07 10:41:08 +0800

    调整工作目录

--- crontab/minute.sh
+++ crontab/minute.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 # 切换工作目录
 cd $(cd `dirname $0`; pwd)
+cd ..
 # 每分钟执行脚本，获取货币最新报价
 /usr/bin/php think crontab:coin:quotes
--- crontab/minutes.sh
+++ crontab/minutes.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 # 切换工作目录
 cd $(cd `dirname $0`; pwd)
+cd ..
 # 每三分钟执行脚本，更新阈值报警
 /usr/bin/php think crontab:coin:alert
