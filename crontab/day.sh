2b5a035b8a5d6b0517129b6ab6cb1f24582c4135
Lazy <hi@cnlazy.cn>
2020-03-07 11:06:16 +0800

    转换文件编码格式

--- crontab/day.sh
+++ crontab/day.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 # 切换工作目录
-cd $(cd `dirname $0`; pwd)
-cd ..
+cd $(cd `dirname $0`; dirname `pwd`)
 # 每天定时任务
 /usr/bin/php think crontab:coin:rank
 /usr/bin/php think crontab:money:rate
--- crontab/minute.sh
+++ crontab/minute.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 # 切换工作目录
-cd $(cd `dirname $0`; pwd)
-cd ..
+cd $(cd `dirname $0`; dirname `pwd`)
 # 每分钟执行脚本，获取货币最新报价
 /usr/bin/php think crontab:coin:quotes
